cc7f0572b8a7c5e9f130991d533f0a515b636848
devasmith <60352859+devasmith@users.noreply.github.com>
2024-04-07 10:35:59 +0200

    Setup dotfiles for linux machine running fedora
    - Inspired by https://github.com/jonaz/dotfiles

---
+++ bashrc
@@ -0,0 +1,270 @@
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$HOME/.cargo/bin:$PATH:$GOPATH/bin:$HOME/bin
+
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+
+# copied from /etc/bash.bashrc and added alacritty*
+case ${TERM} in
+  xterm*|rxvt*|Eterm|alacritty*|aterm|kterm|gnome*)
+    PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+
+    ;;
+  screen*)
+    PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+    ;;
+esac
+
+
+VIM_COMMAND="vim"
+if type "nvim" > /dev/null; then
+	VIM_COMMAND="nvim"
+fi
+
+#functions for aliases
+customgrepinfile() {
+	rg -i --vimgrep "$1" . | $VIM_COMMAND -c "set noro" -c "noremap <enter> <C-w>F" -c "/$1" -R -
+}
+
+customgrepfilename() {
+	local file
+	file=$(fzf --query="$1" --select-1 --exit-0)
+	[ -n "$file" ] && ${EDITOR:-$VIM_COMMAND} "$file"
+}
+
+# open lastpass for search in fzf and copy selected to clipboard
+p(){
+	lpass show -c --password "$(lpass ls -l |fzf | grep -Po "\[id: (.+)\] " | awk '{print $2}' |  sed 's/\]//g')"
+	echo "Copied password to clipboard"
+}
+
+vimj(){
+	nvim -c 'execute "normal! p"' -c "JsonPretty"
+}
+
+kssh(){
+    if  [ -z "$1" ]; then
+	    node=$(kubectl get node --no-headers=true | fzf | awk '{print $1}')
+    else 
+	    node=$(kubectl --context $1 get node --no-headers=true | fzf | awk '{print $1}')
+	fi
+    ssh $node
+}
+
+mkssh(){
+	nodes=$(kubectl get node --no-headers=true | grep $1 | awk '{print $1}')
+	while IFS= read -r node; do
+		echo "ssh into $node"
+		urxvt -e "bash" -c "ssh $node; bash" &
+	done <<< "$nodes"
+}
+
+git-remove-orphan-branches(){
+	git fetch -p
+	git branch -vv | grep "gone]" | grep -v "\*" | awk '{print $1}' | xargs -r git branch -d
+}
+git-list-orphan-branches(){
+	git fetch -p
+	git branch -vv | grep "gone]" | grep -v "\*"
+}
+
+#aliases
+alias f=customgrepinfile
+alias fn=customgrepfilename
+alias ll="ls -lhstr"
+alias gd="git diff"
+alias open="thunar"
+
+# if we have nvim alias vim to it
+alias vim='nvim'
+alias vimdiff='nvim -d'
+
+
+#settings
+export EDITOR="$VIM_COMMAND"
+export BROWSER=google-chrome-stable
+HISTTIMEFORMAT='%F %T - '
+stty ixany
+stty ixoff -ixon
+HISTCONTROL=ignoreboth
+HISTSIZE=100000
+HISTFILESIZE=10000000
+
+
+# Enable history appending instead of overwriting.
+shopt -s histappend
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -n"
+
+export LPASS_AGENT_TIMEOUT=28800
+
+# Bash won't get SIGWINCH if another process is in the foreground.
+# Enable checkwinsize so that bash will check the terminal size when
+# it regains control.
+# http://cnswww.cns.cwru.edu/~chet/bash/FAQ (E11)
+shopt -s checkwinsize
+
+
+# Set colorful PS1 only on colorful terminals.
+# dircolors --print-database uses its own built-in database
+# instead of using /etc/DIR_COLORS. Try to use the external file
+# first to take advantage of user additions. Use internal bash
+# globbing instead of external grep binary.
+
+# sanitize TERM:
+safe_term=${TERM//[^[:alnum:]]/?}
+match_lhs=""
+
+[[ -f ~/.dir_colors ]] && match_lhs="${match_lhs}$(<~/.dir_colors)"
+[[ -f /etc/DIR_COLORS ]] && match_lhs="${match_lhs}$(</etc/DIR_COLORS)"
+[[ -z ${match_lhs} ]] \
+	&& type -P dircolors >/dev/null \
+	&& match_lhs=$(dircolors --print-database)
+
+
+#export GIT_PS1_SHOWDIRTYSTATE=1
+#export GIT_PS1_SHOWUPSTREAM="auto"
+[[ -f ~/bin/git-prompt.sh ]] && source ~/bin/git-prompt.sh
+if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source "$HOME/.bash-git-prompt/gitprompt.sh"
+fi
+
+k(){
+	if [[ $PS1 = *"kube_ps1"* ]]; then
+		ps1_normal
+	else
+		ps1_kube
+	fi
+}
+
+ps1_normal() {
+	PS1="$(if [[ ${EUID} == 0 ]]; then echo '\[$RED\]\h'; else echo '\[$GREEN\]\u@\h'; fi)\[\$BLUE\] \w \$([[ \$? != 0 ]] && echo \"\[\$RED\]:( \")\[$CYAN\]\$(__git_ps1 '[%s]') \[$BASE2\]\n\$\[$RESET\] "
+}
+
+ps1_kube() {
+	KUBE_PS1_PREFIX="["
+	KUBE_PS1_SUFFIX="]"
+	KUBE_PS1_SYMBOL_ENABLE=false
+	PS1="$(if [[ ${EUID} == 0 ]]; then echo '\[$RED\]\h'; else echo '\[$GREEN\]\u@\h'; fi)\[\$BLUE\] \w \$([[ \$? != 0 ]] && echo \"\[\$RED\]:( \")\[$CYAN\]\$(__git_ps1 '[%s]') \$(kube_ps1) \[$BASE2\]\n\$\[$RESET\] "
+}
+
+if [[ $'\n'${match_lhs} == *$'\n'"TERM "${safe_term}* ]] ; then
+	# we have colors :-)
+
+	# Enable colors for ls, etc. Prefer ~/.dir_colors
+	if type -P dircolors >/dev/null ; then
+		if [[ -f ~/.dir_colors ]] ; then
+			eval $(dircolors -b ~/.dir_colors)
+		elif [[ -f /etc/DIR_COLORS ]] ; then
+			eval $(dircolors -b /etc/DIR_COLORS)
+		fi
+	fi
+    #man pages colors
+    export LESS_TERMCAP_mb=$(echo -e "$RED") # 'blinking' text
+    export LESS_TERMCAP_md=$(echo -e "$BLUE") # 'bold' text
+    export LESS_TERMCAP_us=$(echo -e "$GREEN") # 'underlined' text
+    export LESS_TERMCAP_ue=$(echo -e "$BASE0") # end 'underlined' text
+    export LESS_TERMCAP_so=$(echo -e "$BASE2") # standout mode
+    export LESS_TERMCAP_se=$(echo -e "$BASE0") # end 'standout' mode
+    export LESS_TERMCAP_me=$(echo -e "$BASE0") # end appearance modes
+
+	alias ls="ls --color=auto"
+	alias dir="dir --color=auto"
+	alias grep="grep --color=auto"
+	alias dmesg='dmesg --color'
+	ps1_normal
+
+else
+	# show root@ when we do not have colors
+	PS1="\u@\h \w \$([[ \$? != 0 ]] && echo \":( \")\$ "
+	# Use this other PS1 string if you want \W for root and \w for all other users:
+	# PS1="\u@\h $(if [[ ${EUID} == 0 ]]; then echo '\W'; else echo '\w'; fi) \$([[ \$? != 0 ]] && echo \":( \")\$ "
+
+fi
+
+PS2="> "
+PS3="> "
+PS4="+ "
+
+# Try to keep environment pollution down, EPA loves us.
+unset safe_term match_lhs
+
+# Try to enable the auto-completion (type: "pacman -S bash-completion" to install it).
+[ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
+
+# Try to enable the "Command not found" hook ("pacman -S pkgfile" to install it).
+# See also: https://wiki.archlinux.org/index.php/Bash#The_.22command_not_found.22_hook
+[ -r /usr/share/doc/pkgfile/command-not-found.bash ] && . /usr/share/doc/pkgfile/command-not-found.bash
+
+
+
+# GIT heart FZF
+# -------------
+
+export FZF_DEFAULT_COMMAND="rg --files"
+
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+fzf-down() {
+  fzf --height 50% "$@" --border
+}
+
+gf() {
+  is_in_git_repo || return
+  local file
+  file=$(git -c color.status=always status --short | fzf-down --select-1 -m --ansi --nth 2..,.. --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' | cut -c4- | sed 's/.* -> //')
+  [ -n "$file" ] && ${EDITOR:-$VIM_COMMAND} "$file"
+}
+
+gb() {
+  is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf-down --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+  sed 's/^..//' | cut -d' ' -f1 |
+  sed 's#^remotes/##'
+}
+
+gt() {
+  is_in_git_repo || return
+  git tag --sort -version:refname |
+  fzf-down --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -'$LINES
+}
+
+gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
+gr() {
+  is_in_git_repo || return
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf-down --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
+}
+
+bind '"\er": redraw-current-line'
+bind '"\C-g\C-f": "gf\n"'
+bind '"\C-g\C-b": "$(gb)\e\C-e\er"'
+bind '"\C-g\C-t": "$(gt)\e\C-e\er"'
+bind '"\C-g\C-h": "$(gh)\e\C-e\er"'
+bind '"\C-g\C-r": "$(gr)\e\C-e\er"'
+bind '"\C-g\C-d": "git diff\n"'
+bind '"\C-g\C-s": "git status\n"'
+bind '"\C-v\C-v": "nvim\n"'
+
+
+#fnm
+eval "$(fnm env --shell bash)"
+
---
+++ nvim/lua/config.lua
@@ -0,0 +1,322 @@
+-- Setup nvim-cmp.
+local cmp = require 'cmp'
+local snippy = require("snippy")
+local null_ls = require("null-ls")
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+require('fzf-lua').setup {
+	winopts = {
+		fullscreen = true,
+		border = false,
+	},
+	fzf_opts = {
+		['--layout'] = false,
+		-- ['--info']      = false,
+	},
+}
+
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.nginx_beautifier,
+		null_ls.builtins.formatting.trim_whitespace,
+		null_ls.builtins.formatting.shfmt,
+		null_ls.builtins.diagnostics.staticcheck,
+		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.diagnostics.hadolint,
+		-- null_ls.builtins.diagnostics.phpmd.with({
+		--             extra_args = { os.getenv("HOME")..'/.phpmd-ruleset.xml' },
+		--           }),
+	},
+})
+
+-- nvim-comment stuff
+require('nvim_comment').setup({
+	create_mappings = false,
+	-- line_mapping = '<leader>c<space>',
+	-- operator_mapping = '<leader>c<space>',
+})
+-- i want same mappings for insert/visual
+vim.api.nvim_set_keymap('n', '<leader>c<space>', ':CommentToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>c<space>', ":'<,'>CommentToggle<CR>", { noremap = true, silent = true })
+
+require('nvim-autopairs').setup()
+require('lualine').setup({
+	inactive_sections = {
+		lualine_c = {
+			{
+				'filename',
+				path = 1,
+				shorting_target = 10,
+			}
+		},
+	}
+})
+
+require "fidget".setup {} -- lsp loading info
+require("nvim-surround").setup({})
+require('leap').add_default_mappings()
+
+cmp.setup({
+	preselect = cmp.PreselectMode.None,
+	snippet = {
+		expand = function(args)
+			require('snippy').expand_snippet(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif snippy.can_expand_or_advance() then
+				snippy.expand_or_advance()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif snippy.can_jump(-1) then
+				snippy.previous()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'snippy' },
+		{ name = 'path' },
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lspconfig = require('lspconfig')
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+local servers = {
+	-- TODO css/scss html etc: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tailwindcss
+	clangd = {},
+	rust_analyzer = {},
+	pyright = {},
+	tsserver = {},
+	gopls = {
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+					shadow = true,
+				},
+				staticcheck = true,
+			},
+		},
+	},
+	phpactor = {},
+	vimls = {},
+	jsonnet_ls = {},
+	bashls = {},
+	ansiblels = {
+		-- settings = {
+		-- 	ansible = {
+		-- 		ansible = {
+		-- 			path = "/home/asmith/.local/bin/ansible"
+		-- 		},
+				-- python = {
+				-- 	interpreterPath = '/usr/bin/python3',
+				-- },
+		-- 	},
+		-- },
+	},
+	lua_ls = {
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = 'LuaJIT',
+					-- Setup your lua path
+					path = runtime_path,
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { 'vim' },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+}
+
+
+for server, opts in pairs(servers) do
+	opts.on_attach = on_attach
+	opts.capabilities = capabilities
+	lspconfig[server].setup(opts)
+end
+
+-- treesitter stuff
+require('nvim-treesitter.configs').setup {
+	-- Add languages to be installed here that you want installed for treesitter
+	ensure_installed = {
+		'dockerfile',
+		'c',
+		'cpp',
+		'go',
+		'gomod',
+		'lua',
+		'python',
+		'rust',
+		'typescript',
+		'tsx',
+		'javascript',
+		'vimdoc',
+		'vim',
+		'php',
+		'comment',
+		'yaml',
+		'bash',
+		'jsonnet',
+		'json',
+		'http',
+		'java',
+	},
+
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = true, disable = { 'python' } },
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = '<c-space>',
+			node_incremental = '<c-space>',
+			scope_incremental = '<c-s>',
+			node_decremental = '<c-backspace>',
+		},
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				['aa'] = '@parameter.outer',
+				['ia'] = '@parameter.inner',
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				[']m'] = '@function.outer',
+				[']]'] = '@class.outer',
+			},
+			goto_next_end = {
+				[']M'] = '@function.outer',
+				[']['] = '@class.outer',
+			},
+			goto_previous_start = {
+				['[m'] = '@function.outer',
+				['[['] = '@class.outer',
+			},
+			goto_previous_end = {
+				['[M'] = '@function.outer',
+				['[]'] = '@class.outer',
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				['<leader>a'] = '@parameter.inner',
+			},
+			swap_previous = {
+				['<leader>A'] = '@parameter.inner',
+			},
+		},
+	},
+}
+
+-- require("rest-nvim").setup({
+-- 	env_file = '.requests.env'
+-- })

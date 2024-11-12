4a280f5a661d1da7d7fbaa0bf7a363c1ecf1f5a0
devasmith <60352859+devasmith@users.noreply.github.com>
2024-11-12 16:52:07 +0100

    More i3 tweaks.

--- home/dot_screenlayout/executable_home-multi.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP --primary --mode 1920x1200 --pos 2401x2360 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --mode 3840x2160 --pos 1441x200 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rotate right --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off

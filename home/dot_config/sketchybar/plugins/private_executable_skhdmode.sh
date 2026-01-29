f98e314a79f82d25e167d6c101ce2f508cbbb5ff
devasmith <60352859+devasmith@users.noreply.github.com>
2026-01-29 09:37:18 +0100

    Cleanup old osx stuff, prep for Fedora

--- home/dot_config/sketchybar/plugins/private_executable_skhdmode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-if [[ $SENDER == "skhd_mode_change" ]]; then
-    case $MODE in
-        default) sketchybar --set $NAME label.drawing=off ;;
-        move)    sketchybar --set $NAME label.drawing=on label='[M]' ;;
-        resize)  sketchybar --set $NAME label.drawing=on label='[R]' ;;
-    esac
-fi
